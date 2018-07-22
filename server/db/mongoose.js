3428926e62e4ac790d634c61d92bc0548a7eee0c
Raseshwari <rp3737@rit.edu>
2018-07-22 00:31:36 -0400

    configuring heroku mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //setup Promise
-mongoose.connect('mongodb://raseshwari:raseshwari1@ds143461.mlab.com:43461/todo-nodejs-app');
-//mongoose.connect('mongodb://localhost:27017/TodosApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodosApp');
 
 module.exports = {mongoose}
